0d5ed39c850185e49e6aa54ff28ddd937d5db457
Xelanos <or3232@gmail.com>
2017-08-07 21:28:36 +0300

    DONE, hopefully

--- ex1.c
+++ ex1.c
@@ -9,6 +9,8 @@
 #define QUBED_SQRT 1.5
 #define DECIMAL 10
 #define BUFFER_SIZE 50
+#define PARSING_FIRST "Trouble parsing first line"
+#define PARSING_SECOND "Trouble parsing second line"
 
 long double calculateD1(long double x,long double y)
 {
@@ -96,7 +98,6 @@ int startingDataFromUserInput(long double *x, long double *y, long double *veloc
                               long double *totalTime, long *numberOfSteps, long *stepsToSave)
 {
     int parseResult = 0;
-
     printf("Enter initial position x:\n");
     parseResult += getLongDoubleFromUser(x);
     printf("Enter initial position y:\n");
@@ -113,16 +114,96 @@ int startingDataFromUserInput(long double *x, long double *y, long double *veloc
     parseResult += getLongFromUser(stepsToSave);
     if (parseResult < 0)
     {
-        printf("parsing failed");
         return -1;
     }
     return 0;
 }
 
+char *readLine(char *buffer, int bufferSize,FILE* inputFile)
+{
+    char *cp;
+    if (cp = fgets(buffer,bufferSize,inputFile))
+    {
+        while (*cp == ' ' || *cp == '\t')
+        {
+            cp++;
+        }
+        return cp;
+    }
+    return 0;
+
+}
+
+void parseFirstLine(char *line, long double *x, long double *y, long double *velocityX, long double *velocityY)
+{
+    errno = 0;
+    char *endChar;
+    *x = strtold(line,&endChar);
+    if (*endChar != ',' || errno != 0)
+    {
+        fprintf(stderr,PARSING_FIRST);
+        exit(EXIT_FAILURE);
+    }
+    endChar += 2;
+
+    *y = strtold(endChar,&endChar);
+    if (*endChar != ',' || errno != 0)
+    {
+        fprintf(stderr,PARSING_FIRST);
+        exit(EXIT_FAILURE);
+    }
+    endChar += 2;
+
+    *velocityX = strtold(endChar,&endChar);
+    if (*endChar != ',' || errno != 0)
+    {
+        fprintf(stderr,PARSING_FIRST);
+        exit(EXIT_FAILURE);
+    }
+    endChar += 2;
+
+    *velocityY = strtold(endChar,&endChar);
+    if (*endChar != '\n' || errno != 0)
+    {
+        fprintf(stderr,PARSING_FIRST);
+        exit(EXIT_FAILURE);
+    }
+
+}
+
+void parseSecondLine(char *line, long double *time, long *totalSteps, long *stepsToSave)
+{
+    errno = 0;
+    char *endChar;
+    *time = strtold(line,&endChar);
+    if (*endChar != ',' || errno != 0)
+    {
+        fprintf(stderr,PARSING_SECOND);
+        exit(EXIT_FAILURE);
+    }
+    endChar += 2;
+
+    *totalSteps = strtol(endChar,&endChar,DECIMAL);
+    if (*endChar != ',' || errno != 0)
+    {
+        fprintf(stderr,PARSING_SECOND);
+        exit(EXIT_FAILURE);
+    }
+    endChar += 2;
+
+    *stepsToSave = strtol(endChar,&endChar,DECIMAL);
+    if (*endChar != '\n' || errno != 0)
+    {
+        fprintf(stderr,PARSING_SECOND);
+        exit(EXIT_FAILURE);
+    }
+
+}
 
 int startingDataFromFile(char *inputFileName, long double *x, long double *y, long double *velocityX,
                          long double *velocityY, long double *totalTime, long *numberOfSteps, long *stepsToSave)
 {
+    char buffer[BUFFER_SIZE];
     FILE *inputFile = fopen(inputFileName,"r");
     if (inputFile == NULL)
     {
@@ -130,6 +211,11 @@ int startingDataFromFile(char *inputFileName, long double *x, long double *y, lo
         return errno;
     }
 
+    char* firstLine = readLine(buffer,BUFFER_SIZE,inputFile);
+    parseFirstLine(firstLine, x, y, velocityX, velocityY);
+
+    char* secondLine = readLine(buffer,BUFFER_SIZE,inputFile);
+    parseSecondLine(secondLine, totalTime, numberOfSteps, stepsToSave);
 
     if (fclose(inputFile) != 0)
     {
@@ -172,22 +258,54 @@ int mainLoop(long numbersOfSteps, long whenToPrint, FILE *destinationFile, long 
         return -1;
     }
     return 0;
-    
+
+}
+
+int checkVariableLogic(long double time, long totalSteps, long stepsToSave)
+{
+    if (time <= 0)
+    {
+        fprintf(stderr, "Time cannot be negative or zero!");
+        return -1;
+    }
+
+    if (totalSteps % stepsToSave != 0)
+    {
+        fprintf(stderr, "Steps to save does not divide total steps evenly!");
+        return -1;
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-    long numbersOfSteps = 550, stepsToSave = 110, whenToPrint;
-    long double x, y = 5889, velocityX = 0.0, velocityY = 0, totalTime = 16.544468478, dt;
-    char *destinationFileName;
+    long numbersOfSteps, stepsToSave, whenToPrint;
+    long double x, y , velocityX, velocityY, totalTime, dt;
+    char *destinationFileName = NULL;
     FILE *destinationFile;
 
-    if (argc == 2) {
+    // getting the data.
+    if (argc == 2)
+    {
         destinationFileName = argv[1];
-        startingDataFromUserInput(&x, &y, &velocityX, &velocityY, &totalTime, &numbersOfSteps, &stepsToSave);
-    } else if (argc == 3) {
+        if (startingDataFromUserInput(&x, &y, &velocityX, &velocityY, &totalTime, &numbersOfSteps, &stepsToSave) != 0)
+        {
+            fprintf(stderr,"Parsing of parameters failed");
+            return 1;
+        }
+    } else if (argc == 3)
+    {
         destinationFileName = argv[2];
-        startingDataFromFile(argv[1], &x, &y, &velocityX, &velocityY, &totalTime, &numbersOfSteps, &stepsToSave);
-    } else {
+        if (startingDataFromFile(argv[1], &x, &y, &velocityX,
+                                 &velocityY, &totalTime, &numbersOfSteps, &stepsToSave) != 0)
+        {
+            if (errno == 0)
+            {
+                fprintf(stderr,"Problem parsing from file");
+            }
+            return 1;
+        }
+    } else
+    {
         fprintf(stderr, "Invalid number of arguments");
         return 1;
     }
@@ -197,18 +315,15 @@ int main(int argc, char *argv[]) {
         return errno;
     }
 
-
-    dt = totalTime / numbersOfSteps;
-    if (numbersOfSteps % stepsToSave == 0) {
-        whenToPrint = numbersOfSteps / stepsToSave;
-    } else {
-        fprintf(stderr, "Steps to save does not divide total steps evenly!");
+    //sanity check and final pre-calculation.
+    if (checkVariableLogic(totalTime,numbersOfSteps,stepsToSave) != 0)
+    {
         return 2;
     }
-    printf("x-%Lf,y-%Lf,%Lf,%Lf\n time= %Lf, step =%ld,save=%ld,print when -%ld ", x, y, velocityX, velocityY,
-           totalTime,
-           numbersOfSteps, stepsToSave, whenToPrint);
+    whenToPrint = numbersOfSteps / stepsToSave;
+    dt = totalTime / numbersOfSteps;
 
+    //the good stuff
     mainLoop(numbersOfSteps, whenToPrint, destinationFile, x, y, velocityX, velocityY, dt);
     return 0;
 }
